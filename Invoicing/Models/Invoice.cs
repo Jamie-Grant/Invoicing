a866a9b2e8193443b8376ba67238bf692ecddbc0
Jamie <jamiegrant2002@hotmail.com>
2026-04-02 15:58:56 +0100

    Added Invoice model

---
+++ Invoicing/Models/Invoice.cs
@@ -0,0 +1,19 @@
+﻿namespace Invoicing.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyEmail  { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public List<string> Services { get; set; }
+        public int Amount { get; set; }
+        public int AccountNumber { get; set; }
+        public int SortCode { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
